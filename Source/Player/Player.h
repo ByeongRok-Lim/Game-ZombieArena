eeda08fe962cb8ca73f54273a997a5a1a2d24532
DESKTOP-RA01494\KGA_10 <webqud30@gmail.com>
2022-04-27 17:35:53 +0900

    Insert Bullet, Pick up
    총알, 충돌, 아이템 먹기 추가

--- Source/Player/Player.h
+++ Source/Player/Player.h
@@ -1,14 +1,17 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-
+#include "../Bullet/Bullet.h"
+#include <list>
 using namespace sf;
+
+class PickUp;
 
 class Player
 {
 private:
 
 	const float START_SPEED = 1000;
-	const float START_ACCEL = 5000;
+	const float START_ACCEL = 1500;
 	const float START_HEALTH = 100;
 	const float START_IMMUNE_MS = 200;
 
@@ -33,17 +36,26 @@ private:
 	int health; //현재 hp 게이지 구하기.
 	int maxHealth; //현재상태 최대 hP
 	float immuneMs;
+	bool immune;
 
 	Time lastHit; // 누적시간, 좀비한테 맞고나서의 몇초동안s의 무적시간을 주기위해서..
+
+	const float BULLET_CACHE_SIZE = 200;
+	std::list<Bullet*> unuseBullets;
+	std::list<Bullet*> useBullets;
+	float distanceToMuzzle;
+
 public:
 	Player();
 	~Player();
+
+	void Shoot(Vector2f dir);
 
 	void Spawn(IntRect arena, Vector2i res, int tileSize);
 	bool OnHitted(Time lastHit); //피격시
 
 	Time GetLastTime() const;
-	FloatRect GetGobalBound() const;
+	FloatRect GetGlobalBound() const;
 	Vector2f GetPosition() const;
 	float GetRotation() const;
 	Sprite GetSprite() const;
@@ -51,6 +63,12 @@ public:
 
 	void SetDirection(Vector2f dir);
 	void update(float dt); // 마우스 포지션 들고오기
+
+	bool UpdateCollition(const std::list<PickUp*>& items);
+	bool UpdateCollision(const std::vector<Zombie*>& zombies);
+
+	void Draw(RenderWindow& window);
+
 	void GetHealthItem(int amount);
 	void UpgradeSpeed();
 	void UpgradeMaxHealth();
